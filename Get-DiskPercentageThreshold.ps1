576c4af72ec63e0f56c4ce3ed0dd28bb16f87954
Ajay Acharya <acharyaajay@hotmail.com>
2021-01-30 17:33:56 +0000

    Added some poershell scripts

---
+++ Get-DiskPercentageThreshold.ps1
@@ -0,0 +1,33 @@
+﻿<#
+.SYNOPSIS
+Get-DiskPercentageThreshold retrieves logical disk information from one or
+more computers and works out the free space.
+.DESCRIPTION
+Get-DiskFreeSpace uses WMI to retrieve the Win32_LogicalDisk
+instances from one or more computers. It displays each disk's
+drive letter, free space, total size, and percentage of free
+space.
+.PARAMETER computername
+The computer name, or names, to query. Default: Localhost.
+.PARAMETER drivetype
+The drive type to query. See Win32_LogicalDisk documentation
+for values. 3 is a fixed disk, and is the default.
+.PARAMETER percentage
+The percentage threshold to alert when it is less than a specific amount
+Default: .75
+.EXAMPLE
+Get-DiskPercentageThreshold -computername SERVER-R2 -minimum .75 -drivetype 3
+#>
+
+
+param (
+  $computername = 'localhost',
+  $minpercentfree = 75,
+  $drivetype = 3
+)
+#Convert minimum percent free
+$minpercent = $minpercentfree/100
+
+Get-WmiObject Win32_LogicalDisk -computername $computername  -filter "drivetype=$drivetype" |
+where { ($_.Freespace / $_.Size) -lt $percent} |
+select -Property DeviceID,Freespace,Size
---
+++ get-PhysicalAdapters.ps1
@@ -0,0 +1,25 @@
+﻿<#
+.SYNOPSIS
+Get-PhysicalAdapters Get Physical Network adapters.
+.DESCRIPTION
+Display physical adapters from the win32_networkadapter class.
+.PARAMETER computername
+The computer name to check. 
+.EXAMPLE
+PS C:\Users\Ajay\Documents\scripts> .\get-PhysicalAdapters.ps1 -computername localhost
+#>
+
+[CmdletBinding()]
+param (
+  [Parameter(Mandatory=$True,HelpMessage="Enter a computername to query")]
+  [Alias('hostname')]   
+  [string]$computername
+)
+
+Write-Verbose "Getting physical network adapters from $computername"
+
+Get-WmiObject win32_networkadapter -ComputerName $computername |
+where {$_.physicaladapter} |
+select MACAddress,Adaptertype,DeviceID,Name,Speed
+
+Write-Verbose "Script Finished."
